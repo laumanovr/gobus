3d3e04697075838f67b3fbac0b9656d095f18100
Ramazan <laumanov.r@gmail.com>
2022-08-02 17:17:47 +0600

    create passenger page

---
+++ src/views/manage/AdminPassenger.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="passenger-container">
+    <h2>Пассажиры</h2>
+    <table class="table">
+      <thead>
+      <tr>
+        <th>ФИО</th>
+        <th>Дата Регистрации</th>
+        <th>Номер</th>
+        <th>Почта</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr>
+        <td>Иван Иванов</td>
+        <td>05.07.2022</td>
+        <td>0505123456</td>
+        <td>ivan@mail.com</td>
+      </tr>
+      <tr>
+        <td>Иван Иванов</td>
+        <td>05.07.2022</td>
+        <td>0505123456</td>
+        <td>ivan@mail.com</td>
+      </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+	methods: {
+		// testFunc() {
+		//   // this.$store.dispatch('LoaderStore/setLoader', true);
+		// }
+	}
+};
+</script>
+
+<style lang="scss">
+
+</style>
