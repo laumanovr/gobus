0bcc1a425d41815a0fd361f5722589f4f9a691fe
Ramazan <laumanov.r@gmail.com>
2022-08-02 17:15:25 +0600

    create login-page

---
+++ src/views/LoginPage.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="login-container">
+    <v-form class="login-form" ref="loginForm">
+      <h2>Go Bus Admin</h2>
+      <div>
+        <v-text-field
+          outlined
+          label="Логин"
+          autocomplete="new-password"
+          v-model="loginData.login"
+          :rules="requiredRule"
+        />
+      </div>
+      <div>
+        <v-text-field
+          outlined
+          label="Пароль"
+          type="password"
+          autocomplete="new-password"
+          v-model="loginData.password"
+          :rules="requiredRule"
+        />
+      </div>
+      <div class="align-center">
+        <v-btn color="primary" large class="full-width" @click="submitForm">Войти</v-btn>
+      </div>
+    </v-form>
+  </div>
+</template>
+
+<script>
+import {LoginService} from "@/services/login.service";
+
+export default {
+	data() {
+		return {
+			requiredRule: [(v) => !!v || 'Обязательное поле'],
+			loginData: {
+				login: '',
+				password: ''
+			}
+		};
+	},
+	methods: {
+		async submitForm() {
+			if (this.$refs.loginForm.validate()) {
+			  try {
+					await this.$store.dispatch('LoaderStore/setLoader', true);
+			    await LoginService.onLogin(this.loginData);
+					this.$store.dispatch('LoaderStore/setLoader', false);
+				} catch (err) {
+					console.log(err);
+					this.$store.dispatch('LoaderStore/setLoader', false);
+					this.$toast.error('Неправильный логин или пароль');
+				}
+			}
+		}
+	}
+};
+</script>
+
+<style lang="scss">
+.login-container {
+  height: 100%;
+  background-image: url("/src/assets/images/login-bg.jpg");
+  background-position: center;
+  background-size: cover;
+  .login-form {
+    max-width: 550px;
+    margin: 0 auto;
+    position: relative;
+    top: 50%;
+    transform: translateY(-50%);
+    background: #fff;
+    padding: 20px;
+    border-radius: 10px;
+    box-shadow: 0 0 50px 25px rgba(0,0,0,0.75);
+    -webkit-box-shadow: 0 0 50px 25px rgba(0,0,0,0.75);
+    -moz-box-shadow: 0 0 50px 25px rgba(0,0,0,0.75);
+    h2 {
+      text-align: center;
+      margin-bottom: 15px;
+    }
+  }
+}
+</style>
