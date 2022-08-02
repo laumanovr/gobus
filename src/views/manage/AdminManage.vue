7a673865415658c4f7e32661ca2d0ef44ae6001d
Ramazan <laumanov.r@gmail.com>
2022-08-02 17:15:52 +0600

    create manage component

---
+++ src/views/manage/AdminManage.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="manage-container">
+    <Sidebar />
+    <div class="main-content">
+      <router-view />
+    </div>
+  </div>
+</template>
+
+<script>
+import Sidebar from '@/components/SideBar';
+
+export default {
+	components: {
+		Sidebar
+	}
+};
+</script>
+
+<style lang="scss">
+.manage-container {
+  display: flex;
+  position: relative;
+  .sidebar-container {
+    position: fixed;
+    top: 0;
+    height: 100vh;
+  }
+  .main-content {
+    background: #f6f6f6cf;
+    width: calc(100% - 250px);
+    margin-left: 250px;
+    min-height: 100vh;
+    padding: 30px 20px 50px;
+  }
+}
+</style>
