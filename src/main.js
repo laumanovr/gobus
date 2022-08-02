522f9169e33a0ef1a814da2a710653f5172d8bed
Ramazan <laumanov.r@gmail.com>
2022-08-02 17:13:08 +0600

    add login-bg and main style file

--- src/main.js
+++ src/main.js
@@ -1,13 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import vuetify from './plugins/vuetify';
+import VModal from 'vue-js-modal';
+import './assets/styles/main.scss';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(VModal);
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+	router,
+	store,
+	vuetify,
+	render: h => h(App)
+}).$mount('#app');
