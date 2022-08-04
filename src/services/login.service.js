858f52f810ff07f364792e71154d2c701e611fc5
Ramazan <laumanov.r@gmail.com>
2022-08-04 17:35:48 +0600

    modified login service

--- src/services/login.service.js
+++ src/services/login.service.js
@@ -5,16 +5,21 @@ export class LoginService {
 	constructor() {
 	}
 
-	static onLogin(body) {
-		const url = '';
-		const resp = sendPostRequest(url, body);
-		window.localStorage.setItem('busAdmin', JSON.stringify(resp));
-		router.push('/manage');
-		return resp;
+	static async onLogin(body) {
+	  try {
+			const url = 'login';
+			const resp = await sendPostRequest(url, body);
+			const busAdmin = Object.assign({}, resp.data.user, {token: resp.token});
+			window.localStorage.setItem('busAdmin', JSON.stringify(busAdmin));
+			await router.push('/manage');
+			return resp;
+		} catch (err) {
+			return Promise.reject(err);
+		}
 	}
 
 	static onLogOut() {
 		window.localStorage.removeItem('busAdmin');
-		router.push('/');
+		return router.push('/');
 	}
 }
