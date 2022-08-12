25defb05709e7e93424d8dd24527e0f9e46447bd
Ramazan <laumanov.r@gmail.com>
2022-08-12 18:03:55 +0600

    add delete modal in app vue

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,18 @@
   <v-app id="app">
     <router-view />
     <PreLoader v-if="isLoading" />
+    <DeleteModal />
   </v-app>
 </template>
 
 <script>
 import {mapState} from 'vuex';
+
 export default {
 	name: 'App',
 	components: {
-		PreLoader: () => import('@/components/PreLoader')
+		PreLoader: () => import('@/components/PreLoader'),
+		DeleteModal: () => import('@/components/DeleteModal')
 	},
 	computed: {
 		...mapState('LoaderStore', ['isLoading'])
