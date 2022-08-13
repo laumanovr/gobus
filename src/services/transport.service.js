cf9fbbc8860dc27a6745c5c987c78ed871b139cc
Ramazan <laumanov.r@gmail.com>
2022-08-13 15:06:23 +0600

    create transport service

---
+++ src/services/transport.service.js
@@ -0,0 +1,20 @@
+import {sendGetRequest, sendPostRequest, sendPatchRequest} from "@/services/api.service";
+
+export class TransportService {
+	constructor() {}
+
+	static fetchTransportList() {
+		const url = 'vehicles';
+		return sendGetRequest(url);
+	}
+
+	static create(body) {
+		const url = 'vehicles';
+		return sendPostRequest(url, body);
+	}
+
+	static update(body) {
+		const url = `vehicles/${body.id}`;
+		return sendPatchRequest(url, body);
+	}
+}
