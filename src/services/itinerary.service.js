6d3e990ecc104cc4e24e6bc44f44b3d22daa62b4
Ramazan <laumanov.r@gmail.com>
2022-08-14 12:26:23 +0600

    create itinerary service

---
+++ src/services/itinerary.service.js
@@ -0,0 +1,25 @@
+import {sendGetRequest, sendPostRequest, sendPatchRequest} from "@/services/api.service";
+
+export class ItineraryService {
+	constructor() {}
+
+	static fetchItineraryList() {
+		const url = 'itineraries';
+		return sendGetRequest(url);
+	}
+
+	static create(body) {
+		const url = 'itineraries';
+		return sendPostRequest(url, body);
+	}
+
+	static update(body) {
+		const url = `itineraries/${body.id}`;
+		return sendPatchRequest(url, body);
+	}
+
+	static delete(id) {
+		const url = `itineraries/${id}/archive`;
+		return sendPatchRequest(url);
+	}
+}
