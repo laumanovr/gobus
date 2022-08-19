b370e2662bb41f3291f054095605702a7b69a849
Ramazan <laumanov.r@gmail.com>
2022-08-19 17:22:15 +0600

    add query params for trips

--- src/services/trip.service.js
+++ src/services/trip.service.js
@@ -3,8 +3,11 @@ import {sendGetRequest, sendPatchRequest, sendPostRequest} from "@/services/api.
 export class TripService {
 	constructor() {}
 
-	static fetchTripList() {
-		const url = 'trips';
+	static fetchTripList(date='', driverId='') {
+	  let url = 'trips';
+		if (date) url = `trips?date=${date}`;
+		if (driverId) url = `trips?driverId=${driverId}`;
+		if (date && driverId) url = `trips?date=${date}&driverId=${driverId}`;
 		return sendGetRequest(url);
 	}
 
