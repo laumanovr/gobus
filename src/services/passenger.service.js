51c81a6e235819d1ea8fa57a90e65617798d4839
Ramazan <laumanov.r@gmail.com>
2022-08-24 15:36:04 +0600

    add passenger list in passenger page

--- src/services/passenger.service.js
+++ src/services/passenger.service.js
@@ -1,11 +1,11 @@
 import {sendGetRequest} from "@/services/api.service";
 
 export class PassengerService {
-  constructor() {
-  }
+	constructor() {
+	}
 
-  static fetchPassengerList(page=1) {
-    const url = `users?limit=10&page=${page}`
-    return sendGetRequest(url)
-  }
+	static fetchPassengerList(page=1) {
+		const url = `users?limit=10&page=${page}`;
+		return sendGetRequest(url);
+	}
 }
--- src/views/manage/AdminPassenger.vue
+++ src/views/manage/AdminPassenger.vue
@@ -1,39 +1,65 @@
 <template>
   <div class="passenger-container">
     <h2>Пассажиры</h2>
-    <table class="table">
-      <thead>
-      <tr>
-        <th>ФИО</th>
-        <th>Дата Регистрации</th>
-        <th>Номер</th>
-        <th>Почта</th>
-      </tr>
-      </thead>
-      <tbody>
-      <tr>
-        <td>Иван Иванов</td>
-        <td>05.07.2022</td>
-        <td>0505123456</td>
-        <td>ivan@mail.com</td>
-      </tr>
-      <tr>
-        <td>Иван Иванов</td>
-        <td>05.07.2022</td>
-        <td>0505123456</td>
-        <td>ivan@mail.com</td>
-      </tr>
-      </tbody>
-    </table>
+    <template v-if="passengerList.length">
+      <table class="table">
+        <thead>
+        <tr>
+          <th>№</th>
+          <th>ФИО</th>
+          <th>Дата Регистрации</th>
+          <th>Номер</th>
+          <th>Почта</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for="(passenger, i) in passengerList" :key="i">
+          <td>{{ ((page - 1) * 10) + (i + 1)  }}</td>
+          <td>{{ passenger.surname +' '+ passenger.name }}</td>
+          <td>{{ new Date(passenger.createdAt).toLocaleDateString('ru') }}</td>
+          <td>{{ passenger.mobileNumber }}</td>
+          <td>{{ passenger.email }}</td>
+        </tr>
+        </tbody>
+      </table>
+      <div class="vertical-space"></div>
+      <v-pagination
+        v-model="page"
+        :length="totalUsersCount"
+        :total-visible="10"
+        @input="getPassengerList"
+      />
+    </template>
   </div>
 </template>
 
 <script>
+import {PassengerService} from "@/services/passenger.service";
+
 export default {
+	data() {
+		return {
+			passengerList: [],
+			totalUsersCount: 0,
+			page: 1
+		};
+	},
+	mounted() {
+		this.getPassengerList();
+	},
 	methods: {
-		// testFunc() {
-		//   // this.$store.dispatch('LoaderStore/setLoader', true);
-		// }
+		async getPassengerList() {
+			try {
+				await this.$store.dispatch('LoaderStore/setLoader', true);
+				const resp = await PassengerService.fetchPassengerList(this.page);
+				this.passengerList = resp?.data?.users;
+				this.totalUsersCount = Math.ceil(resp.count / 10);
+				this.$store.dispatch('LoaderStore/setLoader', false);
+			} catch (err) {
+				this.$toast.error(err);
+				this.$store.dispatch('LoaderStore/setLoader', false);
+			}
+		}
 	}
 };
 </script>
