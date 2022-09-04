53a66ae97d2430c85f87fbad4d2a1f32351e9c0d
Ramazan <laumanov.r@gmail.com>
2022-09-04 11:13:29 +0600

    create analytics page data

--- src/views/manage/AdminAnalytics.vue
+++ src/views/manage/AdminAnalytics.vue
@@ -1,5 +1,48 @@
 <template>
   <div class="analytics-container">
     <h2>Аналитика</h2>
+
+    <table class="table">
+      <thead>
+      <tr>
+        <th>Остановка</th>
+        <th>Количество посадок</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="(item, i) in analyticsData" :key="i">
+        <td>{{ item.station ? item.station.name : '' }}</td>
+        <td>{{ item.count }}</td>
+      </tr>
+      </tbody>
+    </table>
   </div>
 </template>
+
+<script>
+import {AnalyticsService} from "@/services/analytics.service";
+
+export default {
+	data() {
+		return {
+			analyticsData: []
+		};
+	},
+	created() {
+		this.getAnalytics();
+	},
+	methods: {
+		async getAnalytics() {
+			try {
+				this.$store.dispatch('LoaderStore/setLoader', true);
+				const resp = await AnalyticsService.fetchStationCount();
+				this.analyticsData = resp.data?.stats;
+				this.$store.dispatch('LoaderStore/setLoader', false);
+			} catch (err) {
+				this.$store.dispatch('LoaderStore/setLoader', false);
+				this.$toast.error(err);
+			}
+		}
+	}
+};
+</script>
