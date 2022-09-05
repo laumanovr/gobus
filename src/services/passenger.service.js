e9162ca075dc3f8c8ce047b56dd0e41c65a181fe
Ramazan <laumanov.r@gmail.com>
2022-09-05 19:25:21 +0600

    add query param for passenger service

--- src/services/passenger.service.js
+++ src/services/passenger.service.js
@@ -4,8 +4,8 @@ export class PassengerService {
 	constructor() {
 	}
 
-	static fetchPassengerList(page=1) {
-		const url = `users?limit=10&page=${page}`;
+	static fetchPassengerList(page=1, queryParam='') {
+		const url = `users?limit=10&page=${page}${queryParam}`;
 		return sendGetRequest(url);
 	}
 }
