16184539fefe006a4d6f54101e2407d8f8724d26
Ramazan <laumanov.r@gmail.com>
2022-09-11 17:10:00 +0600

    add api for all trip bookings

--- src/services/booking.service.js
+++ src/services/booking.service.js
@@ -9,6 +9,11 @@ export class BookingService {
 		return sendGetRequest(url);
 	}
 
+	static fetchAllTripBookings(tripId) {
+	  const url = `trips/${tripId}/seats`;
+	  return sendGetRequest(url);
+	}
+
 	static create(tripId, body) {
 	  const url = `trips/${tripId}/bookings`;
 		return sendPostRequest(url, body);
