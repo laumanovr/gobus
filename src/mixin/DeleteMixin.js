c67064576558eed57af3ec90bc06fe4e4067f614
Ramazan <laumanov.r@gmail.com>
2023-01-12 18:26:53 +0600

    modified delete-mixin

--- src/mixin/DeleteMixin.js
+++ src/mixin/DeleteMixin.js
@@ -2,20 +2,21 @@ import {mapActions, mapState} from "vuex";
 
 export default {
 	computed: {
-		...mapState('RemoverStore', ['deleteItemId', 'selectedService', 'onDelete'])
+		...mapState('RemoverStore', ['deleteItemId', 'selectedService', 'onDelete', 'body'])
 	},
 	methods: {
-		...mapActions('RemoverStore', ['setItemId', 'setSelectedService', 'setDeleteKey']),
+		...mapActions('RemoverStore', ['setItemId', 'setSelectedService', 'setDeleteKey', 'setDeleteBody']),
 
-		async deleteItem(selectedService, id, isConfirm) {
+		async deleteItem(selectedService, id, isConfirm, body='') {
 			if (isConfirm) {
 				this.setSelectedService(selectedService);
 				this.setItemId(id);
+				this.setDeleteBody(body);
 				this.$modal.show('delete-modal');
 			} else {
 				try {
 					await this.$store.dispatch('LoaderStore/setLoader', true);
-					await this.selectedService.delete(this.deleteItemId);
+					await this.selectedService.delete(this.deleteItemId, this.body);
 					await this.setDeleteKey(this.onDelete + 1);
 					await this.$modal.hide('delete-modal');
 					this.$toast.success('Успешно удалено!');
